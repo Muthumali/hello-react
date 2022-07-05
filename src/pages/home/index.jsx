ede91286e1cd3aae553faeaaf02cfb92cadbf641
Muthumali <muthudil1999@gmail.com>
2022-07-05 22:35:39 +0530

    add homepage

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Hello from the Home Page</h1>
+            </div>
+        )
+    }
+}
+
+export default HomePage
